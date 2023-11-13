0a484da2bde1d8c374edf9f214369c8446358773
Nikesh Cohen <you@example.com>
2023-11-13 22:21:49 +0200

    Added count down timer

--- Javascript/script.js
+++
@@ -1,6 +0,0 @@
-const mobileBtn = document.querySelector(".mobile-menu-btn");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-mobileBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("show");
-});
